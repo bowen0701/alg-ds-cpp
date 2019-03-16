e8ca6f3e955ca37e97a21f735992ed96e5f448d9
bowen0701 <bowen0701@gmail.com>
2019-03-16 22:51:41 +0100

    Add space complexity; clean scripts up

--- gcd.cc
+++ gcd.cc
@@ -2,22 +2,22 @@
 using namespace std;
 
 int gcd(int m, int n) {
-  // Greatest Common Divisor (GCD) by Euclid's Algorithm.
+  // Greatest common divisor (GCD) by Euclid's algorithm.
+  //
   // Time complexity: O(m%n).
+  // Space complexity: O(1).
   int r;
-
   while (n != 0) {
     r = m % n;
     m = n;
     n = r;
   }
-
   return m;
 }
 
 int main(){
   int m = 10;
   int n = 4;
-  cout << "gcd: " << gcd(m, n) << endl;
+  cout << "gcd of " << m << ", " << n << ": " << gcd(m, n) << endl;
   return 0;
 }
