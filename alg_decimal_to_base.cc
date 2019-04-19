60fddb6a93be8024236e0e1c7f4f63c607d18bab
bowen0701 <bowen0701@gmail.com>
2019-04-19 23:50:15 +0200

    Add alg_decimal_to_base.cc

---
+++ alg_decimal_to_base.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+long decimal_to_base() {}
+
+int main() {
+	// Binary: (37)_10 = (100101)_2
+    int dec = 37;
+
+    // Hexadecimal: (1728)_10 = (6C0)_16
+    int dec = 1728;
+
+    return 0;
+}
