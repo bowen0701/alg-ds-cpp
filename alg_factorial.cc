94996eb95be66e495ee693babd81b3d1c19cf000
bowen0701 <bowen0701@gmail.com>
2019-05-06 22:28:11 +0200

    Complete FactorialDp()

--- alg_factorial.cc
+++ alg_factorial.cc
@@ -1,6 +1,5 @@
 #include <iostream>
-
-using namespace std;
+#include <vector>
 
 int FactorialRecur(int n) {
   // Factorial by recursion.
@@ -12,69 +11,68 @@ int FactorialRecur(int n) {
   return n * FactorialRecur(n - 1);
 }
 
-long FactorialMemoUtil(int n, int F[]) {
-  // Helper function for factorial_memo().
+int FactorialMemoUtil(int n, int fact[]) {
+// int FactorialMemoUtil(int n, int fact[]) {
+  // Helper function for FactorialMemo().
+  if (fact[n] > 0)
+    return fact[n];
+
   if (n <= 1)
     return 1;
 
-  if (F[n] > 0)
-    return F[n];
-
-  F[n] = n * FactorialMemoUtil(n - 1, F);
-  return F[n];
+  fact[n] = n * FactorialMemoUtil(n - 1, fact);
+  return fact[n];
 }
 
-long FactorialMemo(int n) {
+int FactorialMemo(int n) {
   // Factorial by top-down recursion w/ memoization.
   // Time complexity: O(n).
   // Space complexity: O(n).
-  int F[n];
-  for (int i = 0; i < n; i++) {
-    F[i] = 0;
-  }
-  F[0] = 1;
-  F[1] = 1;
+  int fact[n + 1];
+  for (int i = 0; i <= n; i++)
+    fact[i] = 0;
+  fact[0] = 1;
+  fact[1] = 1;
 
-  return FactorialMemoUtil(n, F);
+  return FactorialMemoUtil(n, fact);
 }
 
-// long factorial_dp(int n) {
-//   // Factorial by bottom-up dynamic programming.
-//   // Time complexity: O(n).
-//   // Space complexity: O(n).
-//   long F[n + 1];
-//   F[0] = 1;
-//   F[1] = 1;
+int FactorialDp(int n) {
+  // Factorial by bottom-up dynamic programming.
+  // Time complexity: O(n).
+  // Space complexity: O(n).
+  std::vector<int> fact(n + 1, 0);
+  fact[0] = 1;
+  fact[1] = 1;
 
-//   for (int i = 2; i <= n; i++)
-//     F[i] = i * F[i - 1];
+  for (int i = 2; i <= n; i++)
+    fact[i] = i * fact[i - 1];
 
-//   return F[n];
-// }
+  return fact[n];
+}
 
-// long factorial_iter(int n) {
+// int FactorialIter(int n) {
 //   // Factorial by bottom-up dynamic programming w/ optimized space.
-//   //
 //   // Time complexity: O(n).
 //   // Space complexity: O(1).
-//   long fn = 1;
+//   int fact = 1;
 
 //   if (n <= 1)
-//     return fn;
+//     return fact;
 //   else
 //     for (int i = 2; i <= n; i++)
-//       fn = i * fn;
+//       fact = i * fact;
 
-//   return fn;
+//   return fact;
 // }
 
 int main() {
   int n = 5;
   
-  cout << "Recur: " << FactorialRecur(n) << endl;
-  cout << "Memo: " << FactorialMemo(n) << endl;
-  // cout << "DP: " << factorial_dp(n) << endl;
-  // cout << "Iter: " << factorial_iter(n) << endl;
+  std::cout << "Recur: " << FactorialRecur(n) << std::endl;
+  std::cout << "Memo: " << FactorialMemo(n) << std::endl;
+  std::cout << "DP: " << FactorialDp(n) << std::endl;
+  // std::cout << "Iter: " << FactorialIter(n) << std::endl;
 
   return 0;
 }
