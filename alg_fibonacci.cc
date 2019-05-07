5cf20f55c9a4ad8125cc9cd72ce1b17dc1e7bd1f
bowen0701 <bowen0701@gmail.com>
2019-05-07 22:19:50 +0200

    Complete FibonacciDp()

--- alg_fibonacci.cc
+++ alg_fibonacci.cc
@@ -2,7 +2,7 @@
 #include <vector>
 
 int FibonacciRecur(int n) {
-  // Fibonacci series by recursion.
+  // Fibonacci series by top-down recursion.
   // Time complexity: O(2^n).
   // Space complexity: O(n).
   if (n <= 1)
@@ -12,6 +12,7 @@ int FibonacciRecur(int n) {
 }
 
 int FibonacciMemoUtil(int n, std::vector<int>& fib) {
+  // Helper function for FibonacciMemo().
   if (fib[n] > 0)
     return fib[n];
 
@@ -23,7 +24,7 @@ int FibonacciMemoUtil(int n, std::vector<int>& fib) {
 }
 
 int FibonacciMemo(int n) {
-  // Fibonacci series by memoization.
+  // Fibonacci series by top-down memoization.
   // Time complexity: O(n).
   // Space complexity: O(n).
   std::vector<int> fib(n + 1, 0);
@@ -33,12 +34,30 @@ int FibonacciMemo(int n) {
   return FibonacciMemoUtil(n, fib);
 }
 
+int FibonacciDp(int n) {
+  // Fibonacci series by bottom-up dynamic programming.
+  // Time complexity: O(n).
+  // Space complexity: O(n).
+  std::vector<int> fib(n + 1, 0);
+  fib[0] = 0;
+  fib[1] = 1;
+
+  if (n <= 1)
+    return n;
+
+  for (int i = 2; i <= n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  
+  return fib[n];
+}
 
 int main() {
   int n = 20;
 
   std::cout << "Recur: " << FibonacciRecur(n) << std::endl;
   std::cout << "Memo: " << FibonacciMemo(n) << std::endl;
+  std::cout << "DP: " << FibonacciDp(n) << std::endl;
 
   return 0;
 }
