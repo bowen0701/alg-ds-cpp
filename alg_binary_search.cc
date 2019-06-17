55e3be6c2a4e3e570a70dd9c3b8e66682e34e7a7
bowen0701 <bowen0701@gmail.com>
2019-06-17 23:53:44 +0200

    Add alg_binary_search.cc

---
+++ alg_binary_search.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+bool BinarySearchIter(std::vector<int>& sorted_nums, int target) {
+  return true;
+}
+
+bool BinarySearchRecur(std::vector<int>& sorted_nums, int target) {
+  return true;
+}
+
+bool BinarySearchRecurFast(std::vector<int>& sorted_nums, int target) {
+  return true;
+}
+
+int main() {
+  return 0;
+}
