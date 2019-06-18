7c3d1f04a028ac67889f81529a46790aeb9b9d34
bowen0701 <bowen0701@gmail.com>
2019-06-18 22:13:53 +0200

    Complete binary search by recursion

--- alg_binary_search.cc
+++ alg_binary_search.cc
@@ -20,7 +20,26 @@ bool BinarySearchIter(std::vector<int>& sorted_nums, int target) {
 }
 
 bool BinarySearchRecur(std::vector<int>& sorted_nums, int target) {
-  return true;
+  if (sorted_nums.size() == 0)
+    return false;
+  else {
+    int mid = sorted_nums.size() / 2;
+    if (sorted_nums[mid] == target)
+      return true;
+    else {
+      if (sorted_nums[mid] < target) {
+        // Slice vector's right part.
+        std::vector<int> sorted_nums_right = std::vector<int>(
+            sorted_nums.begin() + mid + 1, sorted_nums.end());
+        return BinarySearchRecur(sorted_nums_right, target);
+      } else {
+        // Slice vector's left part.
+        std::vector<int> sorted_nums_left = std::vector<int>(
+            sorted_nums.begin(), sorted_nums.begin() + mid);
+        return BinarySearchRecur(sorted_nums_left, target);
+      }
+    }
+  }
 }
 
 bool BinarySearchRecurFast(std::vector<int>& sorted_nums, int target) {
@@ -35,8 +54,8 @@ int main() {
   std::cout << "Iter: " << std::boolalpha <<
       BinarySearchIter(sorted_nums, target) << std::endl;
 
-  // std::cout << "Recur: " << std::boolalpha <<
-  //     BinarySearchRecur(sorted_nums, target) << std::endl;
+  std::cout << "Recur: " << std::boolalpha <<
+      BinarySearchRecur(sorted_nums, target) << std::endl;
 
   return 0;
 }
