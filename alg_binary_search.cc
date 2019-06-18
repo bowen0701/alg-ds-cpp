0200892ee2f49f8cb61bedaf137c14c29276fed2
bowen0701 <bowen0701@gmail.com>
2019-06-18 22:12:37 +0200

    Complete binary search by iteration

--- alg_binary_search.cc
+++ alg_binary_search.cc
@@ -2,7 +2,21 @@
 #include <vector>
 
 bool BinarySearchIter(std::vector<int>& sorted_nums, int target) {
-  return true;
+  int first = 0;
+  int last = sorted_nums.size() - 1;
+  bool is_found = false;
+
+  while (first <= last && !is_found) {
+    int mid = first + (last - first) / 2;
+    if (sorted_nums[mid] == target)
+      is_found = true;
+    else
+      if (sorted_nums[mid] < target)
+        first = mid + 1;
+      else
+        last = mid - 1;
+  }
+  return is_found;
 }
 
 bool BinarySearchRecur(std::vector<int>& sorted_nums, int target) {
@@ -14,5 +28,15 @@ bool BinarySearchRecurFast(std::vector<int>& sorted_nums, int target) {
 }
 
 int main() {
+  std::vector<int> sorted_nums = 
+      {17, 20, 26, 31, 44, 54, 55, 65, 77, 93, 100};
+  int target = 65;
+
+  std::cout << "Iter: " << std::boolalpha <<
+      BinarySearchIter(sorted_nums, target) << std::endl;
+
+  // std::cout << "Recur: " << std::boolalpha <<
+  //     BinarySearchRecur(sorted_nums, target) << std::endl;
+
   return 0;
 }
