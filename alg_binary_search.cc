da91a44db2818de0e0c83804bced1fc3ac9f2024
bowen0701 <bowen0701@gmail.com>
2019-06-19 00:03:21 +0200

    Separate main recursion from base case

--- alg_binary_search.cc
+++ alg_binary_search.cc
@@ -30,22 +30,21 @@ bool BinarySearchRecur(std::vector<int>& sorted_nums, int target) {
   // Space complexity: O(n).
   if (sorted_nums.size() == 0)
     return false;
+
+  int mid = sorted_nums.size() / 2;
+  if (sorted_nums[mid] == target)
+    return true;
   else {
-    int mid = sorted_nums.size() / 2;
-    if (sorted_nums[mid] == target)
-      return true;
-    else {
-      if (sorted_nums[mid] < target) {
-        // Slice vector's right part.
-        std::vector<int> sorted_nums_right = std::vector<int>(
-            sorted_nums.begin() + mid + 1, sorted_nums.end());
-        return BinarySearchRecur(sorted_nums_right, target);
-      } else {
-        // Slice vector's left part.
-        std::vector<int> sorted_nums_left = std::vector<int>(
-            sorted_nums.begin(), sorted_nums.begin() + mid);
-        return BinarySearchRecur(sorted_nums_left, target);
-      }
+    if (sorted_nums[mid] < target) {
+      // Slice vector's right part.
+      std::vector<int> sorted_nums_right = std::vector<int>(
+          sorted_nums.begin() + mid + 1, sorted_nums.end());
+      return BinarySearchRecur(sorted_nums_right, target);
+    } else {
+      // Slice vector's left part.
+      std::vector<int> sorted_nums_left = std::vector<int>(
+          sorted_nums.begin(), sorted_nums.begin() + mid);
+      return BinarySearchRecur(sorted_nums_left, target);
     }
   }
 }
