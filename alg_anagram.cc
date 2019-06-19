73d649f59ded1c5ce61b74818eed25f35edea0e5
bowen0701 <bowen0701@gmail.com>
2019-06-19 23:36:44 +0200

    Add alg_anagram.cc

---
+++ alg_anagram.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+// Anagram: Two strings are anagram if one is a rearrangement of the other.
+
+bool AnagramIter(std::string& str1, std::string& str2) {
+  // Anagram by iteration.
+  return true;
+}
+
+bool AnagramSort(std::string& str1, std::string& str2) {
+  // Anagram by sorting.
+  return true;
+}
+
+bool AnagramCount(std::string& str1, std::string& str2) {
+  // Anagram by counting.
+  return true;
+}
+
+int main() {
+  return 0;
+}
