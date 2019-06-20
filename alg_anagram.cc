b7628a2d8fe173195a1ab02b2f393e41e7b58872
bowen0701 <bowen0701@gmail.com>
2019-06-20 23:10:57 +0200

    Complete anagram by iteration

--- alg_anagram.cc
+++ alg_anagram.cc
@@ -6,7 +6,39 @@
 
 bool AnagramIter(std::string& str1, std::string& str2) {
   // Anagram by iteration.
-  return true;
+  int len_str1 = str1.length();
+  int len_str2 = str2.length();
+
+  // Copy str2 to char array for match memoization.
+  char str2_chars[len_str2];
+  str2.copy(str2_chars, len_str2, 0);
+
+  int pos1 = 0;
+  bool is_match = true;
+
+  while (pos1 < len_str1 && is_match) {
+    int pos2 = 0;
+    bool is_found = false;
+
+    while (pos2 < len_str2 && not is_found) {
+      if (str1[pos1] == str2[pos2]) {
+        is_found = true;
+      } else {
+        pos2++;
+      }
+    }
+
+    if (is_found) {
+      str2_chars[pos2] = '_';
+    } else {
+      is_match = false;
+      break;
+    }
+
+    pos1++;
+  }
+
+  return is_match;
 }
 
 bool AnagramSort(std::string& str1, std::string& str2) {
@@ -20,5 +52,11 @@ bool AnagramCount(std::string& str1, std::string& str2) {
 }
 
 int main() {
+  std::string str1 = "abcd";
+  std::string str2 = "dcba";
+
+  std::cout << "Iter: " << std::boolalpha << 
+      AnagramIter(str1, str2) << std::endl;
+
   return 0;
 }
