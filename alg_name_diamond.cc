6b1a030684341d9ef564edb564715858f599b161
bowen0701 <bowen0701@gmail.com>
2019-09-03 20:58:04 +0200

    Complete name diamond

--- alg_name_diamond.cc
+++ alg_name_diamond.cc
@@ -1,10 +1,43 @@
 #include <iostream>
 
-void NameDiamond() {
-  // Code here.
+// Print name diamond on console.
+// For name = BOWENLI,
+// output:
+// B
+// BO
+// BOW
+// BOWE
+// BOWEN
+// BOWENL
+// BOWENLI
+//  OWENLI
+//   WENLI
+//    ENLI
+//     NLI
+//      LI
+//       I
+
+void NameDiamond(std::string& name) {
+  // Print the 1st half.
+  for (int i = 0; i < name.length(); i++) {
+    std::cout << name.substr(0, i + 1) << std::endl;
+  }
+
+  // Print the 2nd half.
+  for (int i = 0; i < name.length(); i++) {
+    // Print spaces first.
+    for (int j = 0; j < i + 1; j++) {
+      std::cout << " ";
+    }
+    
+    // Print the remaining name chars.
+    std::cout << name.substr(i + 1, name.length() - i) << std::endl;
+  }
 }
 
-
 int main() {
-	return 0;
+  std::string name = "BOWENLI";
+  NameDiamond(name);
+  
+  return 0;
 }
