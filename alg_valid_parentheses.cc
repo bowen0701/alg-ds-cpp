d9b8a2ffae384a07769dee0e9f11b09666632a13
bowen0701 <bowen0701@gmail.com>
2019-09-07 11:45:15 +0200

    Complete valid parentheses w/ time/space complexity

--- alg_valid_parentheses.cc
+++ alg_valid_parentheses.cc
@@ -1,10 +1,66 @@
 #include <iostream>
+#include <map>
+#include <stack>
 #include <string>
 
 bool ValideParentheses(std::string s) {
-	return true;
+  // Valide parentheses.
+  //
+  // Time complexity: O(n), where n is the lenght of s.
+  // Space complexity: O(n).
+  std::string opens = "([{";
+  std::string closes = ")]}";
+
+  std::map<char, char> open_close_map = {
+    {'(', ')'},
+    {'[', ']'},
+    {'{', '}'}
+  };
+
+  // Use stack to collect open parentheses.
+  std::stack<char> stack;
+
+  for (char& c : s) {
+    // Check if char is open parentheses. 
+    if (opens.find(c) != std::string::npos) {
+      stack.push(c);
+      continue;
+    }
+    
+    // Check if char is close parentheses. 
+    if (closes.find(c) != std::string::npos) {
+      // Check if there is open parentheses, return not valid.
+      if (stack.empty())
+        return false;
+
+      // If yes, store stack top and compare close parentheses.
+      char open_c = stack.top();
+      stack.pop();
+
+      if (c != open_close_map[open_c])
+        return false;
+    }
+  }
+
+  // Finally check is there are open parentheses remaining.
+  if (stack.empty())
+    return true;
+  else
+    return false;
 } 
 
 int main() {
-	return 0;
+  // Ans: true
+  std::string s = "(abcd)";
+  std::cout << std::boolalpha << ValideParentheses(s) << std::endl;
+
+  // Ans: true
+  s = "([(a)bcd]{ef}g)";
+  std::cout << std::boolalpha << ValideParentheses(s) << std::endl;
+
+  // Ans: false
+  s = "(ab{c}d]";
+  std::cout << std::boolalpha << ValideParentheses(s) << std::endl;
+
+  return 0;
 }
