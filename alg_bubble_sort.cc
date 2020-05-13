1df399172ca2e226392b49b6c096bc49ba17bbd7
bowen0701 <bowen0701@gmail.com>
2020-05-13 08:24:31 +0200

    Change line for comment

--- alg_bubble_sort.cc
+++ alg_bubble_sort.cc
@@ -8,8 +8,8 @@ void BubbleSort(std::vector<int>& nums) {
   // Space complexity: O(1).
   int n = nums.size();
 
-  // Start from i=n-1,n-2,...,1, swap (nums[j], nums[j+1]), for j=0,...,i-1
-  // if the order is not correct.
+  // Start from i=n-1,n-2,...,1, swap (nums[j], nums[j+1]), 
+  // for j=0,...,i-1, if the order is not correct.
   for (int i = n - 1; i > 0; i--) {
     for (int j = 0; j < i; j ++) {
       if (nums[j] > nums[j + 1]) {
