933730832281099195df6461d322e065718afcd8
bowen0701 <bowen0701@gmail.com>
2020-06-20 18:29:34 +0800

    Revise for loop j++

--- alg_bubble_sort.cc
+++ alg_bubble_sort.cc
@@ -12,7 +12,7 @@ void BubbleSort(std::vector<int>& nums) {
   // Start from i=n-1,n-2,...,1, swap (nums[j], nums[j+1]), 
   // for j=0,...,i-1, if the order is not correct.
   for (int i = n - 1; i > 0; i--) {
-    for (int j = 0; j < i; j ++) {
+    for (int j = 0; j < i; j++) {
       if (nums[j] > nums[j + 1]) {
         int temp = nums[j];
         nums[j] = nums[j + 1];
