f5b2eb241b3e8008d70dc52f152a7871bab36847
bowen0701 <bowen0701@gmail.com>
2020-07-22 19:56:15 +0800

    Design board class

--- tic_tac_toe.cc
+++ tic_tac_toe.cc
@@ -3,9 +3,32 @@
 #include <iostream>
 #include <vector>
 
+const int kNRows = 4;
+const int kNCols = 4;
+const char kCross = 'X';
+const char kCircle = 'O';
+const char kEmpty = ' ';
+
 class Board {
  public:
   Board {
     // Board's constructor.
   }
+
+  void SetNextState(int row, int col, char symbol) {
+    // Set board's next state.
+  }
+
+  char JudgeWinner() {
+    // Judge winner.
+  }
+
+  void ShowBoard() {
+    // Show board.
+  }
+ 
+ private:
+  std::vector<char><std::vector<char>> board(
+    kNRows, std::vector<char>(kNCols, kEmpty));
+  char winner;
 }
