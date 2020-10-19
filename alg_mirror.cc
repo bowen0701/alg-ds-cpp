ba00b2af20a9dd5bf8758dae08fced4ff161aa58
bowen0701 <bowen0701@gmail.com>
2020-10-20 00:17:04 +0100

    Add alg_mirror.cc

---
+++ alg_mirror.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+
+// Mirror a queue and append to it. 
+// Example: ['a', 'b', 'c'] => ['a', 'b', 'c', 'c', 'b', 'a']
+std::queue Mirror(std::queue) {
+	// TODO: implement Mirror().
+}
+
+
+int main() {
+	return 0;
+}
