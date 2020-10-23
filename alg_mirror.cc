9c644aec0912a301d009d017aa53e1784021e935
bowen0701 <bowen0701@gmail.com>
2020-10-23 22:25:50 +0100

    Complete mirror impl

--- alg_mirror.cc
+++ alg_mirror.cc
@@ -1,14 +1,39 @@
 #include <iostream>
-#include <queue>
+#include <deque>
 #include <stack>
 
-// Mirror a queue and append to it. 
-// Example: ['a', 'b', 'c'] => ['a', 'b', 'c', 'c', 'b', 'a']
-std::queue Mirror(std::queue) {
-	// TODO: implement Mirror().
+void Mirror(std::deque<char>& deque) {
+  // Mirror a deque and append to it.
+  // Example: {'a', 'b', 'c'} => {'a', 'b', 'c', 'c', 'b', 'a'}
+  std::stack<char> stack;
+
+  // Pop & push back to deque and use stack to collect reverse items.
+  auto size = deque.size();
+  for (int i = 0; i < size; i++) {
+    auto x = deque.front();
+    stack.push(x);
+    deque.pop_front();
+    deque.push_back(x);
+  }
+
+  // Pop stack to push back reverse items to deque.
+  while (!stack.empty()) {
+    auto x = stack.top();
+    stack.pop();
+    deque.push_back(x);
+  }
 }
 
-
 int main() {
-	return 0;
+    std::deque<char> deque;
+    deque.push_back('a');
+    deque.push_back('b');
+    deque.push_back('c');
+
+    Mirror(deque);
+
+    for (auto x : deque)
+      std::cout << x << std::endl;
+
+    return 0;
 }
