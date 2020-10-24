427a6e51c35de6e8b31d69ae9064e1d950c0caff
bowen0701 <bowen0701@gmail.com>
2020-10-24 11:52:14 +0100

    Add alg_palindrome.cc

---
+++ alg_palindrome.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+bool Palindrome(std::string& s) {
+	// Palindrome: the string is read the same forward & backward.
+	// TODO.
+	return true;
+}
+
+int main() {
+	// Output: True.
+	std::string s1 = "radar";
+
+	// Output: False.
+	std::string s2 = "bowen";
+
+	// Output: True.
+	std::string s3 = "madam";
+
+	return 0;
+}
