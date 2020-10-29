4b5581a5e63454c341a9b28715d7f14cd83a8871
bowen0701 <bowen0701@gmail.com>
2020-10-29 22:54:04 +0000

    Complete alg_print_all_binary.cc

--- alg_print_all_binary.cc
+++ alg_print_all_binary.cc
@@ -1,10 +1,34 @@
 #include <iostream>
+#include <string>
+
+void PrintAllBinaryUtil(int digits, std::string output) {
+  // Base case when digits = 0.
+  if (digits == 0) {
+    std::cout << output << std::endl;
+    return;
+  }
+
+  // Recursive case.
+  PrintAllBinaryUtil(digits - 1, output + "0");
+  PrintAllBinaryUtil(digits - 1, output + "1");
+}
 
 void PrintAllBinary(int digits) {
-  // Print all binary.
+  // Print all binary by backtracking.
   // Example: PrintAllBinary(2): 00, 01, 10, 11.
+  std::string output = "";
+  PrintAllBinaryUtil(digits, output);
 }
 
 int main() {
+  // Output: ""
+  PrintAllBinary(0);
+
+  // Output: "0", "1"
+  PrintAllBinary(1);
+
+  // Output: "000", "001", "010", "011", "100", "101", "110", "111"
+  PrintAllBinary(3);
+
   return 0;
 }
