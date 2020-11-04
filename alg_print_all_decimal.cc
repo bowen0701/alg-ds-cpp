5ca89456f000a2cbacbe10bfb93060b641fb8c7c
bowen0701 <bowen0701@gmail.com>
2020-11-04 07:59:47 +0000

    Revise var to n_digits

--- alg_print_all_decimal.cc
+++ alg_print_all_decimal.cc
@@ -1,26 +1,26 @@
 #include <iostream>
 #include <string>
 
-void PrintAllDecimalUtil(int digits, std::string output) {
-  // Base case when digits = 0.
-  if (digits == 0) {
+void PrintAllDecimalUtil(int n_digits, std::string output) {
+  // Base case when n_digits = 0.
+  if (n_digits == 0) {
     std::cout << output << std::endl;
     return;
   }
 
   // Recursive case.
   for (int i = 0; i < 10; i++) {
-    PrintAllDecimalUtil(digits - 1, output + std::to_string(i));
+    PrintAllDecimalUtil(n_digits - 1, output + std::to_string(i));
   }
 }
 
-void PrintAllDecimal(int digits) {
+void PrintAllDecimal(int n_digits) {
   // Print all decimal by exaustive search.
   // Example: PrintAllBinary(): 00, 01, 02,..., 99.
-  // Time complexity: O(10^n), where n is digits.
+  // Time complexity: O(10^n), where n is n_digits.
   // Space complexity: O(10^n).
   std::string output = "";
-  PrintAllDecimalUtil(digits, output);
+  PrintAllDecimalUtil(n_digits, output);
 }
 
 int main() {
