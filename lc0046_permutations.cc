1435478e11b7276207c023610edbaeb533ccd19a
bowen0701 <bowen0701@gmail.com>
2020-11-05 23:25:27 +0000

    Add lc0046_permutations.cc from corporate house

---
+++ lc0046_permutations.cc
@@ -0,0 +1,30 @@
+/* Leetcode 46. Permutations
+Medium
+
+URL: https://leetcode.com/problems/permutations/
+
+Given a collection of distinct integers, return all possible permutations.
+
+Example:
+Input: [1,2,3]
+Output:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+*/
+
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        // TODO.
+    }
+};
+
+int main() {
+    return 0;
+}
