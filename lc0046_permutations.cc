b4a3b03e210e64d9db9b11642df6df4d2c5bd88d
bowen0701 <bowen0701@gmail.com>
2020-11-07 22:25:20 +0000

    Impl lc0046: permutations

--- lc0046_permutations.cc
+++ lc0046_permutations.cc
@@ -18,13 +18,54 @@
 //   [3,2,1]
 // ]
 
+#include <vector>
+#include <algorithm>
+#include "util.h"
+
+using namespace std;
+
 class Solution {
 public:
+    void permuteUtil(vector<int>& nums,
+                     vector<int>& vec,
+                     vector<std::vector<int>>& result) {
+        // Permute util function: backtracking.
+
+        // Base case.
+        if (vec.size() == nums.size()) {
+            result.push_back(vec);
+            return;
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+            // If nums[i] exists in temp vec, skip nums[i].
+            if (find(vec.begin(), vec.end(), nums[i]) != vec.end())
+                continue;
+
+            // Choose nums[i].
+            vec.push_back(nums[i]);
+
+            // Explore what will follow that.
+            permuteUtil(nums, vec, result);
+
+            // Backtrack by unchoosing nums[i].
+            vec.pop_back();
+        }
+    }
+
     vector<vector<int>> permute(vector<int>& nums) {
-        // TODO.
+        // Permute number vector.
+        vector<int> vec;
+        vector<vector<int>> result;
+        permuteUtil(nums, vec, result);
+        return result;
     }
 };
 
 int main() {
+    vector<int> nums {1, 2, 3};
+    vector<vector<int>> result = Solution().permute(nums);
+    Print2DVector(result);
+
     return 0;
 }
