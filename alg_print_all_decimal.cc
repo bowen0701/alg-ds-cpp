0c0749eee08b1b6c3efc379462f317ec9f8232b9
bowen0701 <bowen0701@gmail.com>
2020-11-12 22:55:07 +0000

    Revise output to result

--- alg_print_all_decimal.cc
+++ alg_print_all_decimal.cc
@@ -1,16 +1,16 @@
 #include <iostream>
 #include <string>
 
-void PrintAllDecimalUtil(int n_digits, std::string output) {
+void PrintAllDecimalUtil(int n_digits, std::string result) {
   // Base case when n_digits = 0.
   if (n_digits == 0) {
-    std::cout << output << std::endl;
+    std::cout << result << std::endl;
     return;
   }
 
   // Recursive case.
   for (int i = 0; i < 10; i++) {
-    PrintAllDecimalUtil(n_digits - 1, output + std::to_string(i));
+    PrintAllDecimalUtil(n_digits - 1, result + std::to_string(i));
   }
 }
 
@@ -19,8 +19,8 @@ void PrintAllDecimal(int n_digits) {
   // Example: PrintAllBinary(): 00, 01, 02,..., 99.
   // Time complexity: O(10^n), where n is n_digits.
   // Space complexity: O(10^n).
-  std::string output = "";
-  PrintAllDecimalUtil(n_digits, output);
+  std::string result = "";
+  PrintAllDecimalUtil(n_digits, result);
 }
 
 int main() {
