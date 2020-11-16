d24c57abbe852ea3f775c07879baa82547ddc23e
bowen0701 <bowen0701@gmail.com>
2020-11-16 22:06:17 +0000

    Fix to use vec

--- util.cc
+++ util.cc
@@ -2,9 +2,9 @@
 
 void PrintVector(std::vector<int>& vec) {
   std::cout << "(";
-  for (int i = 0; i < v.size(); i++) {
-    if (i != v.size() - 1) std::cout << v[i] << ", ";
-    else std::cout << v[i];
+  for (int i = 0; i < vec.size(); i++) {
+    if (i != vec.size() - 1) std::cout << vec[i] << ", ";
+    else std::cout << vec[i];
   }
   std::cout << ")" << std::endl;
 }
