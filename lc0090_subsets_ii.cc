19c046d48c19aee7414cea1ef73634857d95fd60
bowen0701 <bowen0701@gmail.com>
2020-11-18 23:39:33 +0000

    Complete lc0090_subsets_ii.cc

--- lc0090_subsets_ii.cc
+++ lc0090_subsets_ii.cc
@@ -22,16 +22,55 @@ Output:
 */
 
 #include <vector>
+#include <algorithm>
+#include "util.h"
 
 using namespace std;
 
 class Solution {
 public:
+    void subsetsWithDupUtil(vector<int>& nums, 
+                            vector<int>& temp,
+                            vector<vector<int>>& result,
+                            int start) {
+        // Util for subsetsWithDup() by backtracking.
+
+        // Base case.
+        result.push_back(temp);
+
+        // Recursive case: choose i, explore, and backtrack.
+        for (int i = start; i < nums.size(); i++) {
+            if (i > start && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            temp.push_back(nums[i]);
+            subsetsWithDupUtil(nums, temp, result, i + 1);
+            temp.pop_back();
+        }
+    }
+
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        // TODO
+        // Subsets with duplicates.
+        // Time complexity: O(n*2^n).
+        // Space complexity: O(n*2^n).
+
+        // Sort nums to avoid duplicates.
+        sort(nums.begin(), nums.end());
+
+        vector<int> temp;
+        vector<vector<int>> result;
+        int start = 0;
+        subsetsWithDupUtil(nums, temp, result, start);
+        return result;
     }
 };
 
 int main() {
+    // Output = [[2],[1],[1,2,2],[2,2],[1,2],[]]
+    vector<int> nums {2, 1, 2};
+    vector<vector<int>> result = Solution().subsetsWithDup(nums);
+    Print2DVector(result);
+
     return 0;
 }
