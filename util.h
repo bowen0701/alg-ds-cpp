2a9f237d07bbe2d3117d29f4e7118b8b3cddbcbe
bowen0701 <bowen0701@gmail.com>
2020-11-24 00:19:15 +0000

    Impl PrintStringVector() & Print2DStringVector()

--- util.h
+++ util.h
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 void PrintVector(std::vector<int>& vec);
+void PrintStringVector(std::vector<std::string>& vec);
 void Print2DVector(std::vector<std::vector<int>>& vec);
+void Print2DStringVector(std::vector<std::vector<std::string>>& vec);
