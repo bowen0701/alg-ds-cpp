94185974fe38cd727228cfab9181d93726b48f3b
bowen0701 <bowen0701@gmail.com>
2020-11-28 23:24:14 +0000

    Complete quicksort

--- alg_quick_sort.cc
+++ alg_quick_sort.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-
+#include "util.h"
 
 // Quick sort algortihm by recursion.
 //
@@ -12,10 +12,42 @@
 //
 // Time complexity: O(n*logn).
 // Space complexity: O(n).
-void QuickSort(std::vector<int>& nums) {
-  // TODO
+std::vector<int> QuickSort(std::vector<int>& nums) {
+  auto n = nums.size();
+
+  // Base case.
+  if (n <= 1)
+    return nums;
+
+  // Use middle num as pivot to collect small, middle & large numbers.
+  auto pivot = nums[n / 2];
+  std::vector<int> smalls;
+  std::vector<int> middles;
+  std::vector<int> larges;
+  for (auto x : nums) {
+    if (x < pivot) {
+      smalls.push_back(x);
+    } else if (x == pivot) {
+      middles.push_back(x);
+    } else {
+      larges.push_back(x);
+    }
+  }
+
+  // Concate small, middle & large numbers.
+  std::vector<int> sorted_smalls = QuickSort(smalls);
+  std::vector<int> sorted_larges = QuickSort(larges);
+  sorted_smalls.insert(
+    sorted_smalls.end(), middles.begin(), middles.end());
+  sorted_smalls.insert(
+    sorted_smalls.end(), sorted_larges.begin(), sorted_larges.end());
+  return sorted_smalls;
 }
 
 int main() {
+  std::vector<int> nums = {5, 2, 3, 1, 4};
+  std::vector<int> result = QuickSort(nums);
+  PrintVector(result);
+
   return 0;
 }
