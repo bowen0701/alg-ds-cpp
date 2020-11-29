b46330ebc12f1c6f52ab82a272eff09e7cae0b20
bowen0701 <bowen0701@gmail.com>
2020-11-29 07:57:52 +0000

    Revise to template for DRY

--- util.h
+++ util.h
@@ -2,7 +2,24 @@
 #include <vector>
 #include <string>
 
-void PrintVector(std::vector<int>& vec);
-void PrintStringVector(std::vector<std::string>& vec);
-void Print2DVector(std::vector<std::vector<int>>& vec);
-void Print2DStringVector(std::vector<std::vector<std::string>>& vec);
+template<typename T>
+void PrintVector(std::vector<T>& vec) {
+  std::cout << "[";
+  for (int i = 0; i < vec.size(); i++) {
+    if (i != vec.size() - 1) std::cout << vec[i] << ", ";
+    else std::cout << vec[i];
+  }
+  std::cout << "]" << std::endl;
+}
+
+template<typename T>
+void Print2DVector(std::vector<std::vector<T>>& vec) {
+  for (auto v : vec) {
+    std::cout << "[";
+    for (int i = 0; i < v.size(); i++) {
+      if (i != v.size() - 1) std::cout << v[i] << ", ";
+      else std::cout << v[i];
+    }
+    std::cout << "]" << std::endl;
+  }
+}
