ebf23598da2bcbb84917cc2962bce85e1367331c
bowen0701 <bowen0701@gmail.com>
2020-11-29 08:43:40 +0000

    Complete impl for QuickSortInPlace()

--- alg_quick_sort.cc
+++ alg_quick_sort.cc
@@ -16,8 +16,7 @@ std::vector<int> QuickSortByNewVector(std::vector<int>& nums) {
   auto n = nums.size();
 
   // Base case.
-  if (n <= 1)
-    return nums;
+  if (n <= 1) return nums;
 
   // Use middle num as pivot to collect small, middle & large numbers.
   auto pivot = nums[n / 2];
@@ -44,10 +43,68 @@ std::vector<int> QuickSortByNewVector(std::vector<int>& nums) {
   return sorted_smalls;
 }
 
+// Util function for QuickSortInPlace() to rearrange nums in place.
+int Partition(std::vector<int>& nums, int left, int right) {
+  // Use right number as pivot.
+  auto right_num = nums[right];
+
+  // Rearrange numbers w.r.t. pivot: 
+  // - For left <= k <= i: nums[k] <= pivot,
+  // - For i+1 <= k <= j-1: nums[k] > pivot,
+  // - For k = right: nums[k] = pivot.
+  int i = left - 1;
+  for (int j = left; j < right; j++) {
+    if (nums[j] <= right_num) {
+      i++;
+
+      // Swap nums[i] and nums[j].
+      auto temp = nums[i];
+      nums[i] = nums[j];
+      nums[j] = temp;
+    }
+  }
+
+  // Swap nums[i+1] and right number.
+  auto temp = nums[i + 1];
+  nums[i + 1] = nums[right];
+  nums[right] = temp;
+
+  // Return parition position.
+  return i + 1;
+}
+
+// Util function for QuickSortInPlace() by recursion.
+void QuickSortRecur(std::vector<int>& nums, int left, int right) {
+  if (left < right) {
+    auto mid = Partition(nums, left, right);
+    QuickSortRecur(nums, left, mid - 1);
+    QuickSortRecur(nums, mid + 1, right);    
+  }
+}
+
+// Quick sort algorithm by recursion in place.
+//
+// Time complexity: O(n*logn).
+// Space complexity: O(1).
+void QuickSortInPlace(std::vector<int>& nums) {
+  auto n = nums.size();
+
+  // Base case.
+  if (n <= 1) return;
+
+  int left = 0;
+  int right = n - 1;
+  QuickSortRecur(nums, left, right);
+}
+
 int main() {
-  std::vector<int> nums = {5, 2, 3, 1, 4};
-  std::vector<int> result = QuickSortByNewVector(nums);
-  PrintVector(result);
+  std::vector<int> nums1 = {5, 2, 3, 1, 4};
+  std::vector<int> result1 = QuickSortByNewVector(nums1);
+  PrintVector(result1);
+
+  std::vector<int> nums2 = {5, 2, 3, 1, 4};
+  QuickSortInPlace(nums2);
+  PrintVector(nums2);
 
   return 0;
 }
