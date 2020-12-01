6ea7826b38c2f92e521dd543017f2aa08f2a8cb7
bowen0701 <bowen0701@gmail.com>
2020-12-01 23:49:11 +0000

    Write func comments

--- alg_merge_sort.cc
+++ alg_merge_sort.cc
@@ -1,6 +1,15 @@
 #include <iostream>
 #include <vector>
+#include "util.h"
 
+// Merge sort algorithm by divide and conquer.
+//
+// Two merge sorted list methods:
+// - Merge by recursion.
+// - Merge by iteration with two pointers.
+//
+// Time complexity: O(n*logn).
+// Space complexity: O(n).
 void MergeSort(std::vector<int>& nums) {
   // TODO
 }
