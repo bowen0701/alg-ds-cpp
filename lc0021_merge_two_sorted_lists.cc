0acbec3b4450aebcb880b48387b11c9d2cdd58ee
bowen0701 <bowen0701@gmail.com>
2020-12-12 23:39:41 +0000

    Add lc0021_merge_two_sorted_lists.cc

---
+++ lc0021_merge_two_sorted_lists.cc
@@ -0,0 +1,31 @@
+// Leetcode 21. Merge Two Sorted Lists
+// Easy
+//
+// URL: https://leetcode.com/problems/merge-two-sorted-lists/
+//
+// Merge two sorted linked lists and return it as a new list. 
+// The new list should be made by splicing together the nodes of the first two lists.
+//
+// Example:
+// Input: 1->2->4, 1->3->4
+// Output: 1->1->2->3->4->4
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ 
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        
+    }
+};
+
+int main() {
+	return 0;
+}
