0b7f5f32163df28e783ddfb813c480ed6b697b10
bowen0701 <bowen0701@gmail.com>
2020-12-17 23:40:47 +0000

    Add ds_linked_list.cc

---
+++ ds_linked_list.cc
@@ -0,0 +1,13 @@
+// Linked list.
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+int main() {
+  return 0;
+}
