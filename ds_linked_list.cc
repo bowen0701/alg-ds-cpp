c65dc4ffba6650dc27eda213ee4c7a4672f4fc0c
bowen0701 <bowen0701@gmail.com>
2020-12-25 23:54:22 +0000

    Impl constructor & destructor

--- ds_linked_list.cc
+++ ds_linked_list.cc
@@ -2,9 +2,14 @@
 
 #include "ds_linked_list.h"
 
-// TODO
-IntLinkedList::IntLinkedList()
-IntLinkedList::~IntLinkedList()
+IntLinkedList::IntLinkedList() {
+  : head(NULL) {}
+}
+
+IntLinkedList::~IntLinkedList() {
+  while (!IsEmpty()) RemoveFront();
+}
+
 IntLinkedList::IsEmpty() const
 const int& IntLinkedList::front() const
 void IntLinkedList::AddFront(const int& data)
