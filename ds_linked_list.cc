4df9cbef2bdcf082a81fac8bf709390d65d06824
bowen0701 <bowen0701@gmail.com>
2021-01-04 23:40:13 +0000

    Add comments & time/space complexity

--- ds_linked_list.cc
+++ ds_linked_list.cc
@@ -9,10 +9,16 @@ IntLinkedList::~IntLinkedList() {
   while (!IsEmpty()) RemoveFront();
 }
 
+// Check list is empty or not.
+// Time complexity: O(1).
+// Space complexity: O(1).
 bool IntLinkedList::IsEmpty() const {
   return head == NULL;
 }
 
+// Obtain list size.
+// Time complexity: O(n).
+// Space complexity: O(1).
 int IntLinkedList::Size() const {
   int counter = 0;
 
@@ -26,6 +32,9 @@ int IntLinkedList::Size() const {
   return counter;
 }
 
+// Show the list.
+// Time complexity: O(n).
+// Space complexity: O(1).
 void IntLinkedList::Show() const {
   IntNode* current = head;
   while (current) {
@@ -36,12 +45,19 @@ void IntLinkedList::Show() const {
   delete current;
 }
 
+// Prepend data to list head.
+// Time complexity: O(1).
+// Space complexity: O(1).
 void IntLinkedList::AddFront(const int& data) {
   IntNode* new_head = new IntNode(data);
   new_head->next = head;
   head = new_head;
 }
 
+
+// Append data to list tail.
+// Time complexity: O(n).
+// Space complexity: O(1).
 void IntLinkedList::AddBack(const int& data) {
   // Create new node.
   IntNode* new_node = new IntNode(data);
@@ -60,6 +76,9 @@ void IntLinkedList::AddBack(const int& data) {
   current->next = new_node;
 }
 
+// Remove data from list, if existed.
+// Time complexity: O(n).
+// Space complexity: O(1).
 void IntLinkedList::RemoveNode(const int& data) {
   // Edge case: no head.
   if (!head) return;
@@ -79,6 +98,9 @@ void IntLinkedList::RemoveNode(const int& data) {
   }
 }
 
+// Remove front node.
+// Time complexity: O(1).
+// Space complexity: O(1).
 void IntLinkedList::RemoveFront() {
   if (head) {
     IntNode* old_head = head;
@@ -87,6 +109,9 @@ void IntLinkedList::RemoveFront() {
   }
 }
 
+// Insert data to specified position of list.
+// Time complexity = O(pos).
+// Space complexity: O(1).
 void IntLinkedList::Insert(int pos, const int& data) {
   // Edge case: empty head and insert position > 0.
   if (!head && pos > 0) {
@@ -117,6 +142,9 @@ void IntLinkedList::Insert(int pos, const int& data) {
   }
 }
 
+// Pop list node at specified position.
+// Time complexity: O(pos).
+// Space complexity: O(1).
 int IntLinkedList::Pop(int pos = -1) {
   // Edge case: no head.
   if (!head) return 0;
@@ -140,6 +168,9 @@ int IntLinkedList::Pop(int pos = -1) {
   return pop_data;
 }
 
+// Search data in list.
+// Time complexity: O(n).
+// Space complexity: O(1).
 bool IntLinkedList::Search(const int& data) {
   // Edge case: no head.
   if (!head) return false;
@@ -156,6 +187,9 @@ bool IntLinkedList::Search(const int& data) {
   return false;
 }
 
+// Obtain node's index in list.
+// Time complexity: O(n).
+// Space complexity: O(1).
 int IntLinkedList::Index(const int& data) {
   // Edge case: no head.
   if (!head) return -1;
