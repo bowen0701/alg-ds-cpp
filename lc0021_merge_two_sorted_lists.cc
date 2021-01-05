34511e0ff251aeb8696fccc52b44d3d8a8d58e92
bowen0701 <bowen0701@gmail.com>
2021-01-05 23:50:11 +0000

    Revise pointer style and add TODO

--- lc0021_merge_two_sorted_lists.cc
+++ lc0021_merge_two_sorted_lists.cc
@@ -13,16 +13,16 @@
 // Definition for singly-linked list.
 struct ListNode {
     int val;
-    ListNode *next;
+    ListNode* next;
     ListNode() : val(0), next(nullptr) {}
     ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
 };
  
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        
+        // TODO
     }
 };
 
