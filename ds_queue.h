2936fd1b174ba71c7905564e45625970517a512d
bowen0701 <bowen0701@gmail.com>
2021-01-18 08:12:07 +0000

    Add ds_queue.h

---
+++ ds_queue.h
@@ -0,0 +1,20 @@
+#ifndef INT_QUEUE_H_
+#define INT_QUEUE_H_
+
+#include <vector>
+
+class Queue {
+ public:
+  Queue();
+  ~Queue();
+  bool IsEmpty() const;
+  int Peek() const;
+  void Enqueue(int item);
+  int Dequeue();
+  int Size();
+  void Show();
+ private:
+  vector<int> items;
+};
+
+#endif // INT_QUEUE_H_
