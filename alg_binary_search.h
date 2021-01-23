4db1261b8e3b252a0c966bbed094e4df73d38f4c
bowen0701 <bowen0701@gmail.com>
2021-01-23 11:02:25 +0000

    Add header file

---
+++ alg_binary_search.h
@@ -0,0 +1,11 @@
+#ifndef BINARY_SEARCH_H_
+#define BINARY_SEARCH_H_
+
+#include <iostream>
+#include <vector>
+
+bool binarySearchIter(std::vector<int>& sorted_nums, int target);
+bool binarySearchFastRecur(std::vector<int>& sorted_nums, int target);
+bool binarySearchIter(std::vector<int>& sorted_nums, int target);
+
+#endif  // BINARY_SEARCH_H_
