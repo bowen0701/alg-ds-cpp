2f4cfad2b08be5685020e1b308a55298abd0fa25
bowen0701 <bowen0701@gmail.com>
2021-01-31 21:51:28 +0000

    Add ds_stack.h

---
+++ ds_stack.h
@@ -0,0 +1,21 @@
+#ifndef INT_STACK_H_
+#define INT_STACK_H_
+
+#include <vector>
+
+class IntStack {
+ public:
+  IntStack();
+  ~IntStack();
+  bool isEmpty() const;
+  int peek() const;
+  void push(int item);
+  int pop();
+  int size() const;
+  void show() const;
+
+ private:
+  std::vector<int> stack;
+};
+
+#endif INT_STACK_H_
