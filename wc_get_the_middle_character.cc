30d3b27812e1f1e906aaddd2a608d62a5eabfea3
bowen0701 <bowen0701@gmail.com>
2021-06-04 21:59:39 +0100

    Complete wc_get_the_middle_character.cc

---
+++ wc_get_the_middle_character.cc
@@ -0,0 +1,40 @@
+// Codewars: Get the Middle Character
+// 7 kyu
+//
+// URL: https://www.codewars.com/kata/56747fd5cb988479af000028/train/cpp
+//
+// You are going to be given a word. Your job is to return the middle character of the word. 
+// If the word's length is odd, return the middle character. If the word's length is even, 
+// return the middle 2 characters.
+//
+// #Examples:
+//
+// Kata.getMiddle("test") should return "es"
+//
+// Kata.getMiddle("testing") should return "t"
+//
+// Kata.getMiddle("middle") should return "dd"
+//
+// Kata.getMiddle("A") should return "A"
+
+#include <string>
+#include <cassert>
+
+std::string get_middle(std::string input) {
+  int len = input.length();
+  if (len % 2 == 0) {
+    return input.substr(len / 2 - 1, 2);
+  } else {
+    return input.substr(len / 2, 1);
+  }
+}
+
+
+int main() {
+  assert(get_middle("test") == "es");
+  assert(get_middle("testing") == "t");
+  assert(get_middle("middle") == "dd");
+  assert(get_middle("A") == "A");
+
+  return 0;
+}
