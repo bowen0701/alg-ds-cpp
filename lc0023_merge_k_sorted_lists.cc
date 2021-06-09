a3623d09b319fbbe9f90d9eafd820ff03a1e113d
bowen0701 <bowen0701@gmail.com>
2021-06-09 22:02:33 +0100

    Complete SolutionSortAll

--- lc0023_merge_k_sorted_lists.cc
+++ lc0023_merge_k_sorted_lists.cc
@@ -22,8 +22,8 @@
 // Example 2:
 // Input: lists = []
 // Output: []
-// Example 3:
-// 
+//
+// Example 3: 
 // Input: lists = [[]]
 // Output: []
 // 
@@ -35,6 +35,10 @@
 // * lists[i] is sorted in ascending order.
 // * The sum of lists[i].length won't exceed 10^4.
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 // Definition for singly-linked list.
 struct ListNode {
     int val;
@@ -44,13 +48,82 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-class Solution {
+class SolutionSortAll {
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        
+    static bool compare(const ListNode* l1, const ListNode* l2) {
+        return (l1->val < l2->val);
+    }
+
+    ListNode* mergeKLists(std::vector<ListNode*>& lists) {
+        // Append lists's all nodes to vec and sort them.
+        std::vector<ListNode*> vec;
+        for (auto ls : lists) {
+            while (ls) {
+                vec.push_back(ls);
+                ls = ls->next;
+            }
+        }
+        std::sort(vec.begin(), vec.end(), compare);
+
+        // Append null pointer as final node of linked list.
+        vec.push_back(nullptr);
+
+        // Use a dummy node to create a new list based on the sorted nodes.
+        ListNode dummy;
+        ListNode* current = &dummy;
+        for (auto ls : vec) {
+            current->next = ls;
+            current = current->next;
+        }
+        return dummy.next;
     }
 };
 
+void show(ListNode* ls) {
+    while (ls) {
+        std::cout << ls->val;
+        if (ls->next) std::cout << "->";
+        ls = ls->next;
+    }
+}
+
 int main() {
+    // Example 1:
+    // Input: lists = [[1,4,5],[1,3,4],[2,6]]
+    // Output: [1,1,2,3,4,4,5,6]
+    ListNode* l1 = new ListNode(1);
+    l1->next = new ListNode(4);
+    l1->next->next = new ListNode(5);
+
+    ListNode* l2 = new ListNode(1);
+    l2->next = new ListNode(3);
+    l2->next->next = new ListNode(4);
+
+    ListNode* l3 = new ListNode(2);
+    l3->next = new ListNode(6);
+
+    std::vector<ListNode*> lists{l1, l2, l3};
+
+    ListNode* ls = SolutionSortAll().mergeKLists(lists);
+    show(ls); std::cout << std::endl;
+
+    // Example 2:
+    // Input: lists = []
+    // Output: []
+    lists = {};
+
+    ls = SolutionSortAll().mergeKLists(lists);
+    show(ls); std::cout << std::endl;
+
+    // Example 3:   
+    // Input: lists = [[]]
+    // Output: []
+    l1 = nullptr;
+    
+    lists = {l1};
+
+    ls = SolutionSortAll().mergeKLists(lists);
+    show(ls); std::cout << std::endl;
+
     return 0;
 }
