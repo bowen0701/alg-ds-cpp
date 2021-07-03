8302ab3a31d0885ca1cbfec2abb3197d519c243d
bowen0701 <bowen0701@gmail.com>
2021-07-04 00:47:34 +0100

    Replace pointer by reference

--- alg_swap.cc
+++ alg_swap.cc
@@ -1,17 +1,17 @@
 #include <iostream>
 
 // Swap two integers without a 3rd temporary variable.
-void swap(int* x, int* y) {
+void swap(int& x, int& y) {
     if (x == y) return;
-    *x = *x + *y;
-    *y = *x - *y;
-    *x = *x - *y;
+    x = x + y;
+    y = x - y;
+    x = x - y;
 }
 
 int main() {
     int x = 5;
     int y = 10;
-    swap(&x, &y);
+    swap(x, y);
     std::cout << "x = " << x << std::endl;
     std::cout << "y = " << y << std::endl;
     
