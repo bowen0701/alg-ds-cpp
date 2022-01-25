c3c3de3b4acf501c1bcf42426388253afd5c3e64
bowen0701 <bowen0701@gmail.com>
2022-01-25 21:48:10 +0000

    Fix iterating for loop with vector size_t

--- util.h
+++ util.h
@@ -7,7 +7,7 @@
 template<typename T>
 void printVector(std::vector<T>& vec) {
   std::cout << "[";
-  for (int i = 0; i < vec.size(); i++) {
+  for (size_t i = 0; i < vec.size(); i++) {
     if (i != vec.size() - 1) std::cout << vec[i] << ", ";
     else std::cout << vec[i];
   }
@@ -18,7 +18,7 @@ template<typename T>
 void print2DVector(std::vector<std::vector<T>>& vec) {
   for (auto v : vec) {
     std::cout << "[";
-    for (int i = 0; i < v.size(); i++) {
+    for (size_t i = 0; i < v.size(); i++) {
       if (i != v.size() - 1) std::cout << v[i] << ", ";
       else std::cout << v[i];
     }
