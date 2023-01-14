4a8056410ebfb759877b472ef22e9e47931ed919
bowen0701 <bowen0701@gmail.com>
2023-01-14 20:01:26 +0000

    Revise to google c++ style

--- alg_decimal_to_base.cc
+++ alg_decimal_to_base.cc
@@ -6,7 +6,7 @@ const std::string kDigits = "0123456789ABCDEF";
 // Convert decimal to base by iteration.
 // Time complexity: O(d/b).
 // Space complexity: O(d/b).
-std::string decimalToBase(int decimal, int base) {
+std::string DecimalToBase(int decimal, int base) {
   std::vector<int> rem_stack;
   int rem;
 
@@ -16,28 +16,28 @@ std::string decimalToBase(int decimal, int base) {
     decimal = decimal / base;
   }
 
-  std::string base_num = "";
+  std::string base_num_str = "";
 
   while (!rem_stack.empty()) {
     int digit_pos = rem_stack[rem_stack.size() - 1];
-    base_num += kDigits.substr(digit_pos, 1);
+    base_num_str += kDigits.substr(digit_pos, 1);
     rem_stack.pop_back();
   }
 
-  return base_num;
+  return base_num_str;
 }
 
 int main() {
   // Binary: (37)_10 = (100101)_2
   int dec = 37;
   int base = 2;
-  std::cout << "Binary: " << dec << " -> " << decimalToBase(dec, base);
+  std::cout << "Binary: " << dec << " -> " << DecimalToBase(dec, base);
   std::cout << std::endl;
 
   // Hexadecimal: (1728)_10 = (6C0)_16
   dec = 1728;
   base = 16;
-  std::cout << "Hexadecimal: " << dec << " -> " << decimalToBase(dec, base);
+  std::cout << "Hexadecimal: " << dec << " -> " << DecimalToBase(dec, base);
   std::cout << std::endl;
 
   return 0;
