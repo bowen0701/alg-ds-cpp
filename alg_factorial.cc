944d8d8290382ca373541aa8916153cfb53e0ac5
bowen0701 <bowen0701@gmail.com>
2023-01-14 20:03:50 +0000

    Revise to google c++ style

--- alg_factorial.cc
+++ alg_factorial.cc
@@ -4,40 +4,40 @@
 // Factorial by recursion.
 // Time complexity: O(n).
 // Space complexity: O(n).
-int factorialRecur(int n) {
+int FactorialRecur(int n) {
   if (n <= 1)
     return 1;
   
-  return n * factorialRecur(n - 1);
+  return n * FactorialRecur(n - 1);
 }
 
 // Helper function for FactorialMemo().
-int factorialMemoUtil(int n, std::vector<int>& T) {
+int FactorialMemoUtil(int n, std::vector<int>& T) {
   if (T[n] > 0)
     return T[n];
 
   if (n <= 1)
     return 1;
 
-  T[n] = n * factorialMemoUtil(n - 1, T);
+  T[n] = n * FactorialMemoUtil(n - 1, T);
   return T[n];
 }
 
 // Factorial by top-down recursion w/ memoization.
 // Time complexity: O(n).
 // Space complexity: O(n).
-int factorialMemo(int n) {
+int FactorialMemo(int n) {
   std::vector<int> T(n + 1, 0);
   T[0] = 1;
   T[1] = 1;
 
-  return factorialMemoUtil(n, T);
+  return FactorialMemoUtil(n, T);
 }
 
 // Factorial by bottom-up dynamic programming.
 // Time complexity: O(n).
 // Space complexity: O(n).
-int factorialDp(int n) {
+int FactorialDp(int n) {
   std::vector<int> T(n + 1, 0);
   T[0] = 1;
   T[1] = 1;
@@ -51,7 +51,7 @@ int factorialDp(int n) {
 // Factorial by bottom-up dynamic programming w/ optimized space.
 // Time complexity: O(n).
 // Space complexity: O(1).
-int factorialIter(int n) {
+int FactorialIter(int n) {
   int fact = 1;
 
   if (n <= 1)
@@ -66,10 +66,10 @@ int factorialIter(int n) {
 int main() {
   int n = 5;
   
-  std::cout << "Recur: " << factorialRecur(n) << std::endl;
-  std::cout << "Memo: " << factorialMemo(n) << std::endl;
-  std::cout << "DP: " << factorialDp(n) << std::endl;
-  std::cout << "Iter: " << factorialIter(n) << std::endl;
+  std::cout << "Recur: " << FactorialRecur(n) << std::endl;
+  std::cout << "Memo: " << FactorialMemo(n) << std::endl;
+  std::cout << "DP: " << FactorialDp(n) << std::endl;
+  std::cout << "Iter: " << FactorialIter(n) << std::endl;
 
   return 0;
 }
