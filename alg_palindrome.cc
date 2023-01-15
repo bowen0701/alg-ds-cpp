f4e826f6ceff71f78a3553cdfb3a90bb2c377887
bowen0701 <bowen0701@gmail.com>
2023-01-15 12:29:49 +0000

    Revise to google c++ style

--- alg_palindrome.cc
+++ alg_palindrome.cc
@@ -5,7 +5,7 @@
 // Palindrome by recursion.
 // Time complexity: O(n^2), where n is the length of s.
 // Space complexity: O(n).
-bool palindromeRecur(std::string& s) {
+bool PalindromeRecur(std::string& s) {
   // Base case.
   if (s.length() <= 1) {
       return true;
@@ -13,13 +13,13 @@ bool palindromeRecur(std::string& s) {
 
   // Recursive case.
   std::string s_sub = s.substr(1, s.length() - 2);
-  return s[0] == s[s.length() - 1] && palindromeRecur(s_sub);
+  return s[0] == s[s.length() - 1] && PalindromeRecur(s_sub);
 }
 
 // Palindrome by iteration.
 // Time complexity: O(n), where n is the length of s.
 // Space complexity: O(1).
-bool palindromeIter(std::string& s) {
+bool PalindromeIter(std::string& s) {
   // Base case.
   if (s.length() <= 1)
     return true;
@@ -39,18 +39,18 @@ bool palindromeIter(std::string& s) {
 int main() {
   // Output: true.
   std::string s = "radar";
-  std::cout << std::boolalpha << palindromeRecur(s) << std::endl;
-  std::cout << std::boolalpha << palindromeIter(s) << std::endl;
+  std::cout << std::boolalpha << PalindromeRecur(s) << std::endl;
+  std::cout << std::boolalpha << PalindromeIter(s) << std::endl;
 
   // Output: false.
   s = "bowen";
-  std::cout << std::boolalpha << palindromeRecur(s) << std::endl;
-  std::cout << std::boolalpha << palindromeIter(s) << std::endl;
+  std::cout << std::boolalpha << PalindromeRecur(s) << std::endl;
+  std::cout << std::boolalpha << PalindromeIter(s) << std::endl;
 
   // Output: true.
   s = "madam";
-  std::cout << std::boolalpha << palindromeRecur(s) << std::endl;
-  std::cout << std::boolalpha << palindromeIter(s) << std::endl;
+  std::cout << std::boolalpha << PalindromeRecur(s) << std::endl;
+  std::cout << std::boolalpha << PalindromeIter(s) << std::endl;
 
   return 0;
 }
