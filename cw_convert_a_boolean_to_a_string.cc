04dbbef5bb46ca03e08566e4f942444868a6488b
bowen0701 <bowen0701@gmail.com>
2023-09-20 12:22:59 +0100

    Complete codewars Convert a Boolean to a String

---
+++ cw_convert_a_boolean_to_a_string.cc
@@ -0,0 +1,23 @@
+// Codewars: Convert a Boolean to a String
+// 8 kyu
+//
+// URL: https://www.codewars.com/kata/551b4501ac0447318f0009cd/train/cpp
+//
+// Implement a function which convert the given boolean value into its string representation.
+//
+// Note: Only valid inputs will be given.
+
+#include <iostream>
+#include <string>
+
+std::string boolean_to_string(bool b){
+  return b ? "true" : "false";
+}
+
+int main() {
+  assert(boolean_to_string(true) == "true");
+  assert(boolean_to_string(false) == "false");
+
+  return 0;
+}
+
